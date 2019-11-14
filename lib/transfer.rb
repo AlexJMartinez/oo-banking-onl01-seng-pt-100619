ff8016ac4338c3b6b42650eb9c0dca03dd155643
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-14 04:17:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ class Transfer
 
   def reverse_transfer
     if sender.balance += amount
-      sender.balance += amount
+      receiver.balance += amount
       self.status = "reversed"
    end
      binding.pry
